2308c717339e7e41c43df822c682ec7a34920590
Laura <laurao148@gmail.com>
2018-09-29 03:50:08 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,35 +1,25 @@
 class StudentsController < ApplicationController
-
-  def index
-    @students = Student.all
-  end
-
-  def show
-    @student = Student.find(params[:id])
-  end
-
-  def new
+   def new
     @student = Student.new
   end
-
-  def create
+   def create
     @student = Student.new(student_params(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
   end
-
-  def update
+   def show
+    @student = Student.find(params[:id])
+  end
+   def edit
+    @student = Student.find(params[:id])
+  end
+   def update
     @student = Student.find(params[:id])
     @student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
-
-  def edit
-    @student = Student.find(params[:id])
-  end
-
-  def student_params(*args)
+   private
+   def student_params(*args)
     params.require(:student).permit(*args)
   end
-
-end
+ end
