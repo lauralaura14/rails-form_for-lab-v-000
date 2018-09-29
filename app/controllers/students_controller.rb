7fd38ea3eb1158ddf8aceef573e8e8618ffab348
Laura <laurao148@gmail.com>
2018-09-29 03:47:14 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,7 +20,7 @@ class StudentsController < ApplicationController
 
   def update
     @student = Student.find(params[:id])
-    @student = Student.update(student_params(:first_name, :last_name))
+    @student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
